8e37e9ce5d4e654cfb744f5c6a0559c7ae9e4df0
Ricardo Machado <rjfmachado@users.noreply.github.com>
2020-02-04 20:03:46 +0000

    add the year

--- src/terraform/mgWinterReady2020.tf
+++ src/terraform/mgWinterReady2020.tf
@@ -5,7 +5,7 @@ variable "mgWinterReady2020Subscriptions" {
 }
 
 resource "azurerm_management_group" "mgWinterReady2020" {
-  display_name               = "Winter Ready"
+  display_name               = "Winter Ready 2020"
   parent_management_group_id = data.azurerm_management_group.mgTenantRoot.id
   #group_id                   = "winterready"
   #subscription_ids           = var.mgWinterReady2020Subscriptions[var.deploymentStage]
